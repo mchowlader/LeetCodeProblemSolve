4d08bc1cbbb9920d2111d303962b6142eaff6b57
Mithun Chandra Howlader <mithun.howlader222@yahoo.com>
2024-06-29 22:03:21 +0600

    add two problem solutions

---
+++ 26.Remove Duplicates from Sorted Array/RemoveDuplicatesFromSortedArray/RemoveDuplicatesFromSortedArray/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace RemoveDuplicatesFromSortedArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] nums = { 1, 1, 1,2, 3,3 };
+           RemoveDuplicates(nums);
+        }
+
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int i = 1;
+            for (int j = 1; j < nums.Length; j++)
+            {
+                if (nums[i - 1] != nums[j])
+                {
+                    nums[i] = nums[j];
+                    i++;
+                }
+            }
+
+            return i;
+        }
+    }
+}
---
+++ 9.Palindrome Number/PalindromeNumber/PalindromeNumber/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PalindromeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IsPalindrome(123);
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            string input = x.ToString();
+            var len = input.Length-1;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == input[len - i])
+                {
+                    continue;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
